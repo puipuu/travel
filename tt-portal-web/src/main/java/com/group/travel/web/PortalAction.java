318baab3b5227cf8a1ff388b0d24d46d756af2c6
chenhty <1498271355@qq.com>
2018-01-18 11:33:41 +0800

    一

---
+++ tt-portal-web/src/main/java/com/group/travel/web/PortalAction.java
@@ -0,0 +1,21 @@
+package com.group.travel.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PortalAction {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public  String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/{page}",method = RequestMethod.GET)
+    public String toJsp(@PathVariable("page") String page){ return page;}
+
+
+}
